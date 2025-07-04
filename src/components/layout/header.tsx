347f5f21f75b4e222bc592a6eb400928ad1a3836
FutsalDex <futsaldex@gmail.com>
2025-07-04 10:17:10 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -16,6 +16,7 @@ import {
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
 import { useEffect, useState } from 'react';
+import Image from 'next/image';
 
 
 export default function Header() {
@@ -43,11 +44,13 @@ export default function Header() {
     <header className="fixed top-0 left-0 right-0 z-50 bg-primary text-primary-foreground shadow-md">
       <div className="container mx-auto flex h-16 items-center justify-between px-4 md:px-6">
         <Link href="/" className="flex items-center">
-          <img 
+          <Image 
             src="https://i.ibb.co/RTck7Qzq/logo-futsaldex-completo.png" 
             alt="FutsalDex Logo" 
+            width={170}
+            height={48}
             className="h-12 w-auto"
-            crossOrigin="anonymous"
+            priority
           />
         </Link>
         <nav className="hidden items-center space-x-1 md:flex">
