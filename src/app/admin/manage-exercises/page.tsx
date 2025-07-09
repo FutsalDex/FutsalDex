10501053fd26bca9073905d8a52fc6ac3d739e88
FutsalDex <futsaldex@gmail.com>
2025-07-09 06:54:32 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/app/admin/manage-exercises/page.tsx
+++ src/app/admin/manage-exercises/page.tsx
@@ -28,7 +28,7 @@ import { Pagination, PaginationContent, PaginationItem } from "@/components/ui/p
 import { Badge } from "@/components/ui/badge";
 import { Dialog, DialogContent, DialogDescription, DialogFooter, DialogHeader, DialogTitle, DialogTrigger, DialogClose } from "@/components/ui/dialog";
 import { AlertDialog, AlertDialogAction, AlertDialogCancel, AlertDialogContent, AlertDialogDescription, AlertDialogFooter, AlertDialogHeader, AlertDialogTitle } from "@/components/ui/alert-dialog";
-import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
+import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
 import { Textarea } from "@/components/ui/textarea";
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select";
 import { Checkbox } from "@/components/ui/checkbox";
